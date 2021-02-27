98da38773a2bb43e01723ecc7388a81356f7361b
folkfrek <folkfrek@oregonstate.edu>
2021-02-27 02:24:32 -0500

    commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Kyle Folk-Freund on 2/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
